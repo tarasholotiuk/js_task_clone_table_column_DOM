2463e9fe95a2717d932a6629da89d1403731dd75
Taras <taras.holotiuk@gmail.com>
2025-05-19 16:43:06 +0300

    make build

---
+++ index.1173291e.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByTagName("table")[0],l=!0,r=!1,t=void 0;try{for(var n,a=e.rows[Symbol.iterator]();!(l=(n=a.next()).done);l=!0){var o=n.value,c=o.cells[1].cloneNode(!0);o.insertBefore(c,o.cells[o.cells.length-1])}}catch(e){r=!0,t=e}finally{try{l||null==a.return||a.return()}finally{if(r)throw t}}
+//# sourceMappingURL=index.1173291e.js.map
